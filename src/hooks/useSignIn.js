c9b780107ae10463c9e3fd99b6f66bcd046f57a4
Leilei Lee <dreamlllp@163.com>
2020-11-09 10:36:54 +0800

    10.14 storing the acess token step1

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,14 +1,18 @@
 import { useMutation } from "@apollo/react-hooks";
 
 import { AUTHORIZE } from "../graphql/mutations";
+import AuthStorage from '../utils/authStorage';
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHORIZE);
+  const authA = new AuthStorage('authA');
 
   const signIn = async ({ username, password }) => {
     mutate({  variables: { username, password } });
+    await authA.setAccessToken(result.data.authorize.accessToken);
+    const tokenA = await authA.getAccessToken();
+    console.log('tokenA', tokenA);
     console.log('username', username, 'password', password);
-    console.log('Token', result.data.authorize.accessToken);
   };
 
   return [signIn, result];
